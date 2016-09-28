dbaf9f01541575520bdde8e3e956d34f3c5fba50
gaoguoxiang <gaoguoxiang@mobanker.com>
2016-09-28 09:48:44 +0800

    123

--- src/main/java/org/netty/demo/entity/User.java
+++ src/main/java/org/netty/demo/entity/User.java
@@ -1,5 +1,6 @@
 package org.netty.demo.entity;
 
+import lombok.Data;
 import org.framework.basic.entity.BaseEntity;
 
 import javax.persistence.Table;
@@ -7,6 +8,7 @@ import javax.persistence.Table;
 /**
  * Created by snow on 2015/8/20.
  */
+@Data
 @Table(name="t_user")
 public class User extends BaseEntity {
 
