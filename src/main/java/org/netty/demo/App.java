b1cb93372f729c0431dcfc3de9c1bec4d3d50007
gaoguoxiang <snowxuyu@gmail.com>
2016-12-27 21:07:20 +0800

    启动方法默认值修改

--- src/main/java/org/netty/demo/App.java
+++ src/main/java/org/netty/demo/App.java
@@ -7,12 +7,13 @@ import org.framework.basic.server.annotation.NettyBootstrap;
  * Hello world!
  *
  */
-@NettyBootstrap(applicationContext = "classpath:/springs/applicationContext.xml", servletContext = "classpath:/springs/springmvc-servlet.xml")
+@NettyBootstrap(applicationContext = "classpath:/springs/applicationContext.xml",
+        servletContext = "classpath:/springs/springmvc-servlet.xml",
+        serverConfig = "classpath:/configs/server.properties")
 public class App {
     public static void main( String[] args ){
-
-
 		NettyApplication.run(App.class, args);
+
     	// 创建->设置application父容器->设置springmvc子容器->设置server参数->启动
       /*  NettyServerBootstrap.createServer()
         	.setSpringApplicationContext("classpath:/springs/applicationContext.xml")
