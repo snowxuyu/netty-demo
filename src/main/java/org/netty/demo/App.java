d4f9af0cfcc73b2259b2aa3c5928efa813799863
gaoguoxiang <snowxuyu@gmail.com>
2017-03-04 22:50:01 +0800

    update

--- src/main/java/org/netty/demo/App.java
+++ src/main/java/org/netty/demo/App.java
@@ -13,12 +13,5 @@ import org.framework.basic.server.annotation.NettyBootstrap;
 public class App {
     public static void main( String[] args ){
 		NettyApplication.run(App.class, args);
-
-    	// 创建->设置application父容器->设置springmvc子容器->设置server参数->启动
-      /*  NettyServerBootstrap.createServer()
-        	.setSpringApplicationContext("classpath:/springs/applicationContext.xml")
-        	.setSpringServletContext("classpath:/springs/springmvc-servlet.xml")
-        	.parseCommondArguments(args)
-        	.start();*/
     }
 }
